0fd093a76da2114cedea1c1445b5b79e829a8095
Mateusz Zając <matzayonc@gmail.com>
2024-02-24 15:13:02 +0100

    added svelte browser app

---
+++ browser/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Web Transport Web Client</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>Web Transport Chat App</h1>
+</section>
